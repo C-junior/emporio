47672e6da7fcc832fa6b46dba8c06d315b1a0db9
C-junior <dkjunior13@gmail.com>
2023-02-03 18:17:13 -0300

    online urls and tabs

--- main.js
+++ main.js
@@ -49,25 +49,129 @@
         //     <img title="Freyja" src="https://fire-emblem-heroes.com/common/img/sp/index/characterSlide/character_thumb_27.png" alt="">
         // </li>
 
-        let promocoes = document.querySelector(".btn")
-        let divProduct = document.querySelector("#cards")
-    
-        fetch("dados.json").then((response) => {
-            response.json().then((dados) => {
-              
-                dados.products.map((product) => {
+      let url = 'https://api.jsonbin.io/v3/b/63dd69c3c0e7653a056e79bf';
+        let divProduct = document.querySelector("#promo") 
+        let divAll = document.querySelector("#all") 
+        let divBebidas = document.querySelector("#bebidas") 
+        let divChoco = document.querySelector("#choco") 
+
+         //todas
+  fetch(url).then((response) => {
+    response.json().then((dados) => {
+        console.log(dados)
+        dados.record.products.map((product) => {
                     if (product.disponivel == 1){
-                        divProduct.innerHTML += `
+                        divAll.innerHTML += `
                     <ul>
                         <li class= "card">
-                            <img title="${product.nome} " class="food" src="./assets/${product.img}.jpg" alt=""></img><div class="content">  <h3> ${product.nome}  </h3> <p> ${product.desc} </p> <div class="price">${product.price} </div></div>
+                            <img title="${product.nome} " class="food" src= ${product.img} alt=""></img><div class="content">  <h3> ${product.nome}  </h3> <p> ${product.desc} </p> <div class="price">${product.price} </div></div>
                             </li>
                         </ul>
                     `
                 }
+            
                   
                 })       
-            //     dados.products.map((product) => {
+         
+            })
+        })
+        
+        //promo
+        fetch(url).then((response) => {
+            response.json().then((dados) => {
+                console.log(dados)
+                dados.record.products.map((product) => {
+                    if (product.promocao == 13){
+                    if (product.disponivel == 1){
+                        divProduct.innerHTML += `
+                        <ul>
+                        <li class= "card">
+                            <img title="${product.nome} " class="food" src= ${product.img} alt=""></img><div class="content">  <h3> ${product.nome}  </h3> <p> ${product.desc} </p> <div class="price">${product.price} </div></div>
+                            </li>
+                        </ul>
+                    `
+                }
+            }
+                  
+                })       
+         
+            })
+        })
+  //bebidas
+  fetch(url).then((response) => {
+    response.json().then((dados) => {
+        console.log(dados)
+        dados.record.products.map((product) => {
+                    if (product.promocao == 1){
+                    if (product.disponivel == 1){
+                        divBebidas.innerHTML += `
+                        <ul>
+                        <li class= "card">
+                            <img title="${product.nome} " class="food" src= ${product.img} alt=""></img><div class="content">  <h3> ${product.nome}  </h3> <p> ${product.desc} </p> <div class="price">${product.price} </div></div>
+                            </li>
+                        </ul>
+                    `
+                }
+            }
+                  
+                })       
+         
+            })
+        })
+//choco
+fetch(url).then((response) => {
+    response.json().then((dados) => {
+        console.log(dados)
+        dados.record.products.map((product) => {
+                    if (product.promocao == 2){
+                    if (product.disponivel == 1){
+                        divChoco.innerHTML += `
+                        <ul>
+                        <li class= "card">
+                            <img title="${product.nome} " class="food" src= ${product.img} alt=""></img><div class="content">  <h3> ${product.nome}  </h3> <p> ${product.desc} </p> <div class="price">${product.price} </div></div>
+                            </li>
+                        </ul>
+                    `
+                }
+            }
+                  
+                })       
+         
+            })
+        })
+ 
+        
+
+        //tabs
+
+        function openCity(evt, cityName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+              tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+              tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(cityName).style.display = "block";
+            evt.currentTarget.className += " active";
+          }
+          
+          // Get the element with id="defaultOpen" and click on it
+          document.getElementById("defaultOpen").click();
+    
+        // function promocoes() {
+        //     document.getElementById("cards").innerHTML = `
+        //     <ul>
+        //     <li class= "card">
+        //         <img title="${product.nome} " class="food" src="/assets/${product.img}.jpg" alt=""></img><div class="content">  <h3> ${product.nome}  </h3> <p> ${product.desc} </p> <div class="price">${product.price} </div></div>
+        //         </li>
+        //     </ul>
+        // `
+        // }
+
+           //     dados.products.map((product) => {
             //         document.getElementById('btn').onclick = function()  {
             //         if (product.promocao == "b"){
             //             divProduct.innerHTML += `
@@ -81,18 +185,6 @@
             // }
                   
             //     })   
-            })
-        })
-
-        // function promocoes() {
-        //     document.getElementById("cards").innerHTML = `
-        //     <ul>
-        //     <li class= "card">
-        //         <img title="${product.nome} " class="food" src="/assets/${product.img}.jpg" alt=""></img><div class="content">  <h3> ${product.nome}  </h3> <p> ${product.desc} </p> <div class="price">${product.price} </div></div>
-        //         </li>
-        //     </ul>
-        // `
-        // }
 
         // filterbtn
       
